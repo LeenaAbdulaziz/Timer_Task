22b166961fdc6363c79524d2010b10095a192552
lamia-fd <90754742+lamia-fd@users.noreply.github.com>
2021-11-03 14:16:50 +0300

    the recycler update

---
+++ app/src/main/java/com/example/timer_task/recyclerView.kt
@@ -0,0 +1,37 @@
+package com.example.timer_task
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.card_task.view.*
+
+class recyclerView (private val activity: Activity, private val taskContant: ArrayList<ArrayList<String>>): RecyclerView.Adapter<recyclerView.ItemHolder>() {
+    class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        return ItemHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_task, parent, false))
+    }
+
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+
+        holder.itemView.apply {
+           // task.text = taskContant[position][0]
+           /// desc.text= taskContant[position][1]
+           // time.text=taskContant[position][2]
+
+            card.setOnClickListener {
+
+               /// Alert()
+
+            }
+
+
+        }
+    }
+
+    override fun getItemCount()= taskContant.size
+
+}
